46caffdf176a23fb83c795881a55e51526a1b7b9
hungit119 <tranduyhungdz119@gmail.com>
2022-09-02 14:43:08 +0700

    Films suggetions on detail page done

--- src/controllers/filmController.js
+++ src/controllers/filmController.js
@@ -7,6 +7,150 @@ class FilmController {
   index(req, res) {
     res.json({ message: "success" });
   }
+  // [GET] /api/films/suggest/:id
+  selectFilmsSuggest(req, res) {
+    const { id } = req.params;
+    const query = `select film.title,types.title as type,
+    group_concat(distinct genres.title order by genres.title separator ", ") as genres,
+    group_concat(distinct casts.title order by casts.title separator ", ") as casts,
+    group_concat(distinct productions.title order by productions.title separator ", ") as productions
+    from film
+    inner join film_genre on film_genre.film_id = film.id
+    inner join genres on genres.id = film_genre.genre_id
+    inner join film_cast on film_cast.film_id = film.id
+    inner join casts on casts.id = film_cast.cast_id
+    inner join film_production on film_production.film_id = film.id
+    inner join productions on productions.id = film_production.production_id
+    inner join types on types.id = film.type_id
+    where film.id = '${id}'
+    group by film.title;`;
+    con.query(query, function (error, rows) {
+      if (error) {
+        res.status(400).json({ success: false, message: "query error" });
+        throw error;
+      } else {
+        const titleKey = rows[0].title;
+        const typeKey = rows[0].type;
+        const genreKey = rows[0].genres
+          .split(",")
+          .map((genre) => `'${genre.trim()}'`);
+        const castKey = rows[0].casts
+          .split(",")
+          .map((cast) => `'${cast.trim()}'`);
+        const productioKey = rows[0].productions
+          .split(",")
+          .map((production) => `'${production.trim()}'`);
+        let query = `select film.stt,film.id,film.title,film.poster,film.trailerURL,film.thumnail,film.times,film.description,film.tags,film.rating,film.imdb,film.releases,film.director,types.title as type,quantities.title as quantity,years.title as year,
+        group_concat(distinct genres.title order by genres.title separator ", ") as genres,
+        group_concat(distinct countries.title order by countries.title separator ", ") as countries,
+        group_concat(distinct casts.title order by casts.title separator ", ") as casts,
+        group_concat(distinct productions.title order by productions.title separator ", ") as productions,film.up_to_date
+        from film
+        inner join film_genre on film_genre.film_id = film.id
+        inner join genres on genres.id = film_genre.genre_id
+        inner join film_country on film_country.film_id = film.id
+        inner join countries on countries.id = film_country.country_id
+        inner join film_cast on film_cast.film_id = film.id
+        inner join casts on casts.id = film_cast.cast_id
+        inner join film_production on film_production.film_id = film.id
+        inner join productions on productions.id = film_production.production_id
+        inner join types on types.id = film.type_id
+        inner join quantities on quantities.id = film.quantity_id
+        inner join years on years.id = film.year_id
+        WHERE (film.title not like '${titleKey}' and types.title = '${typeKey}' and (
+          casts.title in (${castKey}) or (productions.title in (${productioKey}) and genres.title in (${genreKey}))
+        ))
+        group by film.title
+        order by film.stt desc
+        ${req.query.limit ? `LIMIT ${req.query.limit}` : ""};`;
+        con.query(query, function (error, rows) {
+          if (error) {
+            throw error;
+          } else {
+            res.json({
+              success: true,
+              message: "Get films suggest successfully",
+              filmSuggests: rows,
+            });
+          }
+        });
+      }
+    });
+  }
+  // [GET] /api/films/lastestMovies
+  selectLastest(req, res) {
+    const query = `select film.stt,film.id,film.title,film.poster,film.trailerURL,film.thumnail,film.times,film.description,film.tags,film.rating,film.imdb,film.releases,film.director,types.title as type,quantities.title as quantity,years.title as year,
+    group_concat(distinct genres.title order by genres.title separator ", ") as genres,
+    group_concat(distinct countries.title order by countries.title separator ", ") as countries,
+    group_concat(distinct casts.title order by casts.title separator ", ") as casts,
+    group_concat(distinct productions.title order by productions.title separator ", ") as productions,film.up_to_date
+    from film
+    inner join film_genre on film_genre.film_id = film.id
+    inner join genres on genres.id = film_genre.genre_id
+    inner join film_country on film_country.film_id = film.id
+    inner join countries on countries.id = film_country.country_id
+    inner join film_cast on film_cast.film_id = film.id
+    inner join casts on casts.id = film_cast.cast_id
+    inner join film_production on film_production.film_id = film.id
+    inner join productions on productions.id = film_production.production_id
+    inner join types on types.id = film.type_id
+    inner join quantities on quantities.id = film.quantity_id
+    inner join years on years.id = film.year_id
+    where types.title = '${req.params.type}'
+    group by film.title
+    order by up_to_date desc
+    ${req.query.limit ? `LIMIT ${req.query.limit}` : ""};`;
+
+    con.query(query, function (error, rows) {
+      if (error) {
+        res.status(400).json({ success: false, message: error.message });
+        throw error;
+      } else {
+        res.json({
+          success: true,
+          message: "get films lastest success",
+          filmsType: rows,
+        });
+      }
+    });
+  }
+  // [GET] /api/films/:condition/:key/:value
+  selectByCondition(req, res) {
+    const { condition, key, value } = req.params;
+    const query = `select film.stt,film.id,film.title,film.poster,film.trailerURL,film.thumnail,film.times,film.description,film.tags,film.rating,film.imdb,film.releases,film.director,types.title as type,quantities.title as quantity,years.title as year,
+    group_concat(distinct genres.title order by genres.title separator ", ") as genres,
+    group_concat(distinct countries.title order by countries.title separator ", ") as countries,
+    group_concat(distinct casts.title order by casts.title separator ", ") as casts,
+    group_concat(distinct productions.title order by productions.title separator ", ") as productions,film.up_to_date
+    from film
+    inner join film_genre on film_genre.film_id = film.id
+    inner join genres on genres.id = film_genre.genre_id
+    inner join film_country on film_country.film_id = film.id
+    inner join countries on countries.id = film_country.country_id
+    inner join film_cast on film_cast.film_id = film.id
+    inner join casts on casts.id = film_cast.cast_id
+    inner join film_production on film_production.film_id = film.id
+    inner join productions on productions.id = film_production.production_id
+    inner join types on types.id = film.type_id
+    inner join quantities on quantities.id = film.quantity_id
+    inner join years on years.id = film.year_id
+    where ${key.toLowerCase()} ${condition} '${value}'
+    group by film.title
+    order by film.stt
+    ${req.query.limit ? `LIMIT ${req.query.limit}` : ""};`;
+    con.query(query, function (error, rows) {
+      if (error) {
+        res.status(400).json({ success: false, message: error.message });
+        throw error;
+      } else {
+        res.json({
+          success: true,
+          message: "get films by condition success",
+          filmsType: rows,
+        });
+      }
+    });
+  }
   // [GET] /api/films/type/:params
   filmsType(req, res) {
     const query = `select film.stt,film.id,film.title,film.poster,film.trailerURL,film.thumnail,film.times,film.description,film.tags,film.rating,film.imdb,film.releases,film.director,types.title as type,quantities.title as quantity,years.title as year,
@@ -26,10 +170,12 @@ class FilmController {
       inner join types on types.id = film.type_id
       inner join quantities on quantities.id = film.quantity_id
       inner join years on years.id = film.year_id
-      where types.title = '${req.params.params}'
+      where types.title = '${req.params.params}' ${
+      req.query.year ? `and years.title = ${req.query.year}` : ""
+    }
       group by film.title
       order by film.stt
-      LIMIT 24;`;
+      ${req.query.limit ? `LIMIT ${req.query.limit}` : ""};`;
     con.query(query, function (err, results) {
       if (err) throw err;
       res.json({
--- src/routes/filmRoute.js
+++ src/routes/filmRoute.js
@@ -2,16 +2,20 @@ const express = require("express");
 const filmController = require("../controllers/filmController");
 const Router = express.Router();
 
-// [GET] /api/films
-Router.get("/films", filmController.read);
+//[GET] /api/films/:condition/key/value
+Router.get("/films/:condition/:key/:value", filmController.selectByCondition);
 // [GET] /api/films/type/:params
 Router.get("/films/type/:params", filmController.filmsType);
 // [GET] /api/film/:id
 Router.get("/film/:id", filmController.film);
-// [GET] /api/film/:id
-Router.get("/film/:id", filmController.film);
 // [POST] /api/film/create
 Router.post("/film/create", filmController.create);
+// [GET] /api/films/lastest/:type
+Router.get("/films/lastest/:type", filmController.selectLastest);
+// [GET] /api/films/suggest/:id
+Router.get("/films/suggest/:id", filmController.selectFilmsSuggest);
+// [GET] /api/films
+Router.get("/films", filmController.read);
 // [GET] /api
 Router.get("/", filmController.index);
 
